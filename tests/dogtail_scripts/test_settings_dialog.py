23d93337a12060c85ccd4d0cc9dccd120394d984
Matas Brazdeikis <matas@brazdeikis.lt>
2012-06-27 14:27:04 +0100

    Reworked tests

--- tests/dogtail_scripts/test_settings_dialog.py
+++ tests/dogtail_scripts/test_settings_dialog.py
@@ -1,41 +1,46 @@
 #!/usr/bin/env python
 
-from dogtail.config import config
+import unittest
+from test_basic import BaseDogTail
+from dogtail.predicate import IsATextEntryNamed
+import hashlib
 
-from dogtail.utils import screenshot, run
-from dogtail.tree import *
-from dogtail.predicate import *
-import os
-import thread
 
-run('bin/pitivi', 10)
-pitivi = root.application('pitivi')
-#Just create new project
-pitivi.child(name="New", roleName='push button').click()
+class SettingDialogTest(BaseDogTail):
+    def test_settings(self):
+        #Just create new project
+        self.pitivi.child(name="New", roleName='push button').click()
 
-#Play with project settings, look if they are correctly represented
-dialog = pitivi.child(name="Project Settings", roleName="dialog")
-video = pitivi.tab("Video")
+        #Play with project settings, look if they are correctly represented
+        dialog = self.pitivi.child(name="Project Settings", roleName="dialog")
+        video = self.pitivi.tab("Video")
 
-#Test presets
-video.child(name="720p24", roleName="table cell").click()
-children = video.findChildren(IsATextEntryNamed(""))
-childtext = {}
-for child in children:
-        childtext[child.text] = child
+        #Test presets
+        video.child(name="720p24", roleName="table cell").click()
+        children = video.findChildren(IsATextEntryNamed(""))
+        childtext = {}
+        for child in children:
+                childtext[child.text] = child
 
-assert("1:1" in childtext)
-assert("24M" in childtext)
-assert("16:9" in childtext)
+        self.assertIn("1:1", childtext)
+        self.assertIn("24M", childtext)
+        self.assertIn("16:9", childtext)
 
-#Test frame rate combinations
-frameCombo = video.child(name="23.976 fps", roleName="combo box")
-frameText = childtext["24M"]
-frameCombo.click()
-video.child(name="59.94 fps", roleName="menu item").click()
-assert (frameText.text == "60M")
-frameText.typeText("30M")
-video.child(name="29.97 fps", roleName="combo box")
+        #Test frame rate combinations
+        frameCombo = video.child(name="23.976 fps", roleName="combo box")
+        frameText = childtext["24M"]
+        frameCombo.click()
+        video.child(name="120 fps", roleName="menu item").click()
+        self.assertEqual(frameText.text, "120:1")
+        frameText.click()
+        frameText.typeText("0")
+        video.child(name="12 fps", roleName="combo box")
 
-#Create project
-pitivi.child(name="OK", roleName="push button").click()
+        #Create project
+        self.pitivi.child(name="OK", roleName="push button").click()
+        self.saveAsProject("/tmp/settings.xptv")
+        #TODO: save project and than checksum it with good one
+        #print hashlib.md5(file("/tmp/settings.xptv", 'r').read()).digest()
+
+if __name__ == '__main__':
+    unittest.main()
