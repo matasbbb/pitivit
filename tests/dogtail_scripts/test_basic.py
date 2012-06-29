79c6a821f22065f6972cb70d2831a6eafb1abe9e
Matas Brazdeikis <matas@brazdeikis.lt>
2012-06-29 09:55:02 +0100

    Few more tests (not working due to dogtail errors)

---
+++ tests/dogtail_scripts/test_basic.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+import os
+
+
+class BaseDogTail(unittest.TestCase):
+    def setUp(self):
+        #Try to speed up a little
+        from dogtail.config import config
+        config.load({'actionDelay': 0.1,
+                     'typingDelay': 0.01,
+                     'runTimeout': 3,
+                     'searchCutoffCount': 5,
+                     'defaultDelay': 0.05})
+        from dogtail.utils import run
+        from dogtail.tree import root
+        #Run pitivi
+        self.pid = run('bin/pitivi', dumb=True)
+        #If pitivi is not runned, tests are skipped
+        self.pitivi = root.application('pitivi')
+
+    @unittest.skip("helper function")
+    def saveAsProject(self, url):
+        self.pitivi.menu("Project").click()
+
+        #FIXME: cant get working with Save As…
+        self.pitivi.menu("Project").children[4].click()
+        saveas = self.pitivi.child(roleName='dialog')
+        saveas.child(roleName='text').text = url
+
+        #Click the Save button.
+        saveas.button('Save').click()
+
+    @unittest.skip("helper function")
+    def loadProject(self, url, save=False):
+        self.pitivi.menu("Project").click()
+        self.pitivi.menu("Project").children[2].click()
+        load = self.pitivi.child(roleName='dialog')
+        load.child(name="Type a file name", roleName="toggle button").click()
+        load.child(roleName='text').text = url
+        load.button('Load').click()
+        try:
+            if save:
+                load.child(name="Close without saving", roleName="push button")
+        except:
+            return
+
+    def tearDown(self):
+        #Try to kill pitivi before leaving test
+        os.system("kill -9 %i" % self.pid)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/dogtail_scripts/test_timeline.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import unittest
+from test_basic import BaseDogTail
+from dogtail.predicate import GenericPredicate
+import dogtail.rawinput
+import os
+from time import sleep
+
+
+class TimelineTest(BaseDogTail):
+
+    def test_import_clip(self):
+        self.pitivi.child(name="New", roleName='push button').click()
+        self.pitivi.child(name="OK", roleName="push button").click()
+        self.pitivi.child(name="Import Files...",
+                          roleName="push button").click()
+        add = self.pitivi.child(roleName='dialog')
+        add.child(name="Type a file name", roleName="toggle button").click()
+        filepath = os.path.realpath(__file__).split("dogtail_scripts/test_timeline.py")[0]
+        filepath += "samples/1sec_simpsons_trailer.mp4"
+        add.child(roleName='text').text = filepath
+        add.button('Add').click()
+        icons = self.pitivi.findChildren(GenericPredicate(roleName="icon"))
+        sample = None
+        for icon in icons:
+            if icon.text == "1sec_simpsons_trailer.mp4":
+                sample = icon
+
+        self.assertIsNotNone(sample)
+        return sample
+
+    def test_insert_at_end(self):
+        sample = self.test_import_clip()
+        #Right click
+        texts = self.pitivi.findChildren(GenericPredicate(roleName="text"))
+        seektime = None
+        for text in texts:
+            if text.text == "0:00:00.000":
+                seektime = text
+
+        self.assertIsNotNone(seektime)
+
+        sample.click(3)
+        buttons = self.pitivi.findChildren(
+            GenericPredicate(name="Insert at End of Timeline"))
+        buttons[1].click()
+        self.pitivi.child(name="Next", roleName="push button").click()
+        self.assertEqual(seektime.text, "0:00:01.227")
+
+        #Add one more
+        sample.click(3)
+        buttons = self.pitivi.findChildren(
+            GenericPredicate(name="Insert at End of Timeline"))
+        buttons[1].click()
+        self.pitivi.child(name="Next", roleName="push button").click()
+
+        self.assertEqual(seektime.text, "0:00:02.455")
+
+    def test_drag_clip(self):
+        sample = self.test_import_clip()
+        #Right click
+        texts = self.pitivi.findChildren(GenericPredicate(roleName="text"))
+        seektime = None
+        for text in texts:
+            if text.text == "0:00:00.000":
+                seektime = text
+
+        self.assertIsNotNone(seektime)
+        self.pitivi.findChildren(GenericPredicate(roleName="layered pane"))
+        timeline = self.pitivi.children[0].children[0].children[2].children[1].children[3]
+        dogtail.rawinput.drag((sample.position[0] + sample.size[0] / 2,
+                               sample.position[1] + sample.size[1] / 2),
+                              (timeline.position[0] + timeline.size[0] / 2,
+                               timeline.position[1] + timeline.size[1] / 2))
+        sleep(2)
+        self.pitivi.child(name="Next", roleName="push button").click()
+        self.assertNotEqual(seektime.text, "0:00:00.000")
+
+
+if __name__ == '__main__':
+    unittest.main()
