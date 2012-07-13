1e598faef17e3efdcc61731a2be32517420e4f4e
Matas Brazdeikis <matas@brazdeikis.lt>
2012-07-13 14:43:47 +0100

    Few more tests

--- tests/dogtail_scripts/test_timeline.py
+++ tests/dogtail_scripts/test_timeline.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-
 import unittest
 from test_basic import BaseDogTail
 from dogtail.predicate import GenericPredicate
 from helper_functions import help_test_import_media, drag
 import dogtail.rawinput
 from time import sleep
+from pyatspi import Registry as registry
+from pyatspi import (KEY_SYM, KEY_PRESS, KEY_PRESSRELEASE, KEY_RELEASE)
 
 
 class TimelineTest(BaseDogTail):
@@ -54,7 +55,7 @@ class TimelineTest(BaseDogTail):
         timeline = self.pitivi.children[0].children[0].children[2].children[1].children[3]
 
         #Adjust to different screen sizes
-        adj = (float)(timeline.size[0]) / 900
+        adj = (float)(timeline.size[0]) / 883
 
         dogtail.rawinput.click(timeline.position[0] + 500 * adj, timeline.position[1] + 50)
         self.pitivi.child(name="Split", roleName="push button").click()
@@ -70,6 +71,23 @@ class TimelineTest(BaseDogTail):
         self.pitivi.child(name="Next", roleName="push button").click()
         self.assertEqual(seektime.text, "0:00:01.227")
 
+    def test_multiple_split(self):
+        self.help_test_insertEnd()
+        seektime = self.search_by_text("0:00:02.455", self.pitivi, roleName="text")
+        timeline = self.pitivi.children[0].children[0].children[2].children[1].children[3]
+        #Adjust to different screen sizes
+        adj = (float)(timeline.size[0]) / 883
+        tpos = timeline.position
+        pos = [50, 480, 170, 240, 350, 610, 410, 510]
+        #Sleeps needed for atspi
+        for k in pos:
+            for p in pos:
+                dogtail.rawinput.click(tpos[0] + (p + k / 10) * adj, tpos[1] + 50)
+                sleep(0.3)
+                dogtail.rawinput.pressKey("s")
+                #Just search some object to look if it still alive
+                self.pitivi.child(roleName="icon")
+
     def test_transition(self):
         self.help_test_insertEnd()
         seektime = self.search_by_text("0:00:02.455", self.pitivi, roleName="text")
@@ -77,7 +95,7 @@ class TimelineTest(BaseDogTail):
         tpos = timeline.position
 
         #Adjust to different screen sizes
-        adj = (float)(timeline.size[0]) / 900
+        adj = (float)(timeline.size[0]) / 883
 
         dogtail.rawinput.press(tpos[0] + 500 * adj, tpos[1] + 50)
         #Drag in, drag out, drag in and release
@@ -97,6 +115,67 @@ class TimelineTest(BaseDogTail):
         iconlist.children[-2].select()
         self.assertTrue(transitions.child(roleName="slider").sensitive)
         transitions.child(roleName="slider").value = 50
+
+    def search_clip_end(self, y, seek, timeline):
+        minx = timeline.position[0] + 10
+        maxx = timeline.position[0] + timeline.size[0] - 10
+        minx = (minx + maxx) / 2
+        y += timeline.position[1]
+        dogtail.rawinput.click(maxx, y)
+        maxseek = seek.text
+        print maxseek
+        while maxx - minx > 2:
+            middle = (maxx + minx) / 2
+            dogtail.rawinput.click(middle, y)
+            sleep(0.1)
+            if seek.text == maxseek:
+                maxx = middle
+            else:
+                minx = middle
+
+        return maxx - timeline.position[0]
+
+    def test_riple_roll(self):
+        self.help_test_insertEnd()
+        seektime = self.search_by_text("0:00:02.455", self.pitivi, roleName="text")
+        timeline = self.pitivi.children[0].children[0].children[2].children[1].children[3]
+        tpos = timeline.position
+        end = self.search_clip_end(30, seektime, timeline)
+
+        dogtail.rawinput.absoluteMotion(tpos[0] + end / 2 - 2, tpos[1] + 30)
+        registry.generateKeyboardEvent(dogtail.rawinput.keyNameToKeyCode("Control_L"), None, KEY_PRESS)
+        dogtail.rawinput.press(tpos[0] + end / 2 - 2, tpos[1] + 30)
+        sleep(0.5)
+        dogtail.rawinput.absoluteMotion(tpos[0] + end / 2 - 100, tpos[1] + 30)
+        sleep(0.5)
+        dogtail.rawinput.release(tpos[0] + end / 2 - 100, tpos[1] + 30)
+        registry.generateKeyboardEvent(dogtail.rawinput.keyNameToKeyCode("Control_L"), None, KEY_RELEASE)
+        self.pitivi.child(name="Next", roleName="push button").click()
+        self.assertNotEqual(seektime.text, "0:00:02.455", "Not ripled, but trimed")
+
+        #Regresion test of adding effect
+        #Add effect
+        tab = self.pitivi.tab("Effect Library")
+        tab.click()
+        conftab = self.pitivi.tab("Clip configuration")
+        conftab.click()
+        center = lambda obj: (obj.position[0] + obj.size[0] / 2, obj.position[1] + obj.size[1] / 2)
+        table = conftab.child(roleName="table")
+        icon = self.search_by_text("Agingtv ", tab, roleName="icon")
+        drag(center(icon), center(table))
+        self.pitivi.child(name="Next", roleName="push button").click()
+        seekbefore = seektime.text
+        #Try riple and roll
+        dogtail.rawinput.absoluteMotion(tpos[0] + end / 2 - 102, tpos[1] + 30)
+        registry.generateKeyboardEvent(dogtail.rawinput.keyNameToKeyCode("Control_L"), None, KEY_PRESS)
+        dogtail.rawinput.press(tpos[0] + end / 2 - 102, tpos[1] + 30)
+        sleep(0.5)
+        dogtail.rawinput.absoluteMotion(tpos[0] + end / 2 - 200, tpos[1] + 30)
+        sleep(0.5)
+        dogtail.rawinput.release(tpos[0] + end / 2 - 200, tpos[1] + 30)
+        registry.generateKeyboardEvent(dogtail.rawinput.keyNameToKeyCode("Control_L"), None, KEY_RELEASE)
+        self.pitivi.child(name="Next", roleName="push button").click()
+        self.assertNotEqual(seektime.text, seekbefore, "Not ripled affter adding effect")
 
 if __name__ == '__main__':
     unittest.main()
